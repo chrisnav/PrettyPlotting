ca8fba0985c594c5476d7d01a9a085d55ef0a472
DESKTOP-8KC59B1\chris <christian.oyn.naversen@gmail.com>
2021-04-25 13:35:44 +0200

    Added som metadata to setup.py

--- setup.py
+++ setup.py
@@ -10,4 +10,11 @@ setup(
     author_email='christian.oyn.naversen@gmail.com',
     install_requires=['matplotlib>=3.4.1'],
     license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows :: Windows 10',        
+        'Programming Language :: Python :: 3.9',
+    ],    
 )
